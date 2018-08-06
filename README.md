2222d466b855986ea6aa4b07532be6d2fcaa9e08
leoEspin <leonardo.x.espin@gmail.com>
2018-08-06 16:33:22 -0400

    improved parallel bash script

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Execute **heat_demo.sh** to run a complete simulation of the 2D heat equation on
 
 The script will ask you to generate an input file (click ok for the defaults) and then it will compile and run the main code. The results will be plotted and stored in *Heated tile.pdf*.
 
-
+Execute **parallel_heat.sh** to test execution speed ups with multithreading. 
 
--- parallel_heat.sh
+++ parallel_heat.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 #
-#  Compile the program with G++
+#  Compile the program with g++
 #
 echo "g++ compiler should be installed for this script to work"
 g++ -fopenmp heat_equation.cpp -lm -o heat_p
+file="input"
+if [ ! -f $file ]; then
+    python gimmick.py
+fi
 #
 #  Run with 1, 2, and 4 threads.
 #
-echo "Run with 1 thread."
-export OMP_NUM_THREADS=1
-./heat_p > output.txt
-#
-echo "Run with 2 threads."
-export OMP_NUM_THREADS=2
-./heat_p >> output.txt
-#
-echo "Run with 4 threads."
-export OMP_NUM_THREADS=4
-./heat_p >> output.txt
+rm -f output.txt
+for i in {1,2,4};
+do
+    echo "Run with $i threads"
+    export OMP_NUM_THREADS=$i
+    ./heat_p >> output.txt
+done
 #
 #  Discard the executable file.
 #
